1af96ff9c72f267c732459810e32314cf6d97066
hershler <hra@sem-wolf.org>
2020-07-23 00:06:44 +0300

    improve the prints to be nicer

--- src/--code--/start_app.py
+++ src/--code--/start_app.py
@@ -1,16 +1,27 @@
 from complete_to_sentences import init_system, find_best_k_completions
 
+k = 5
+
+
+def print_completions(the_completions):
+    m = min(len(the_completions), k)
+
+    if not the_completions:
+        print("no suggestions :(")
+    else:
+        print(f"Here are the {m} suggestions")
+        for i in range(1, m + 1):
+            print(f"{i}. {completions[i - 1]}")
+
 
 if __name__ == "__main__":
-    print("starting...\n")
+    print("Loading the files and preparing the system\n...")
     init_system()
-    print("finished\n")
+    print("the system is ready \n")
     while True:
-        input_ = input("please")
-        while input_[-1] != '#':
-            # validate_input()
-            completions = find_best_k_completions(input_)
-            print(f"completions: {completions}\n{input_}", end="")
-            input_ += input()
-
-
+        input_ = input("please enter a text\n")
+        if input_:
+            while input_[-1] != '#':
+                completions = find_best_k_completions(input_)
+                print_completions(completions)
+                input_ += input()
