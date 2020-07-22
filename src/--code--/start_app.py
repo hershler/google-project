99c7e696bfa70cdf98a409e3de697d969fffdd8a
hershler <hra@sem-wolf.org>
2020-07-22 12:24:28 +0300

    add the main function where the application's flow start

---
+++ src/--code--/start_app.py
@@ -0,0 +1,16 @@
+from ..complete_to_sentences import init_system, find_best_k_completions
+
+
+if __name__ == "__main__":
+    print("starting...\n")
+    init_system()
+    print("finished\n")
+    while True:
+        input_ = input("please\n")
+        while input_[-1] != '#':
+            # validate_input()
+            completions = find_best_k_completions(input_)
+            print(f"completions: {completions}\n{input_}")
+            input_ += input()
+
+
