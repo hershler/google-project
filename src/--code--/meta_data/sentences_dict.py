f333424306c20d965afe6b6706098d78e9a1e79d
hershler <hra@sem-wolf.org>
2020-07-23 11:27:24 +0300

    add worrking with real data - reading sentences from files

--- src/--code--/meta_data/sentences_dict.py
+++ src/--code--/meta_data/sentences_dict.py
@@ -1,9 +1,18 @@
+import glob
+
 
 # has to be an object, while changing remember go to sub_string_trie.init - values
-sentences_dict = {0: "hello big and nice world"}
+sentences_dict = {}
 
 
 def init_sentences_dict():
     # read sentences from files
-    pass
+
+    for filename in glob.iglob("../--data--technology_texts/python-3.8.4-docs-text/" + '/**.txt', recursive=True):
+
+        with open(filename, mode='r', encoding='utf-8') as f:
+            sentences = f.readlines()
+            sentences = [x.strip() for x in sentences]
+            for i in range(len(sentences)):
+                sentences_dict[i] = sentences[i]
 
