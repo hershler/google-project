d2186aacc0275c46892aa7f70e569cb317877ca6
hershler <hra@sem-wolf.org>
2020-07-23 11:25:45 +0300

    fix bug

--- src/--code--/utils.py
+++ src/--code--/utils.py
@@ -1,13 +1,16 @@
+
 def normal_string(string):
     ret = ""
     length = len(string)
 
     for i in range(length):
+
         if string[i].isalpha():
             ret += string[i]
         elif ' ' == string[i] and i:
             if ' ' != string[i - 1]:
                 ret += ' '
-    if ' ' == ret[-1]:
-        ret = ret[:-2]
-    return ret.lower()
+    ret2 = ret
+    if ret and ' ' == ret[-1]:
+        ret2 = ret[:-2]
+    return ret2.lower()
